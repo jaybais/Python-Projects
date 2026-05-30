cdcab2102cbcad9a538ef201f3cea526e4d8fcb8
Jay Bais <81273683+jaybais@users.noreply.github.com>
2026-05-30 14:03:55 -0400

    created config.py - API is loading in Python

---
+++ Weather-App/config.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+API_KEY = os.getenv("OPENWEATHER_API_KEY")
+
+if not API_KEY:
+    raise ValueError("No API key found. Make sure your .env file has OPENWEATHER_API_KEY set.")
